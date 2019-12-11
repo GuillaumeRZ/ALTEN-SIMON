83be013285ab65405260d79fa5ff528c69c5972f
Simon PERRIN <tox6mon@gmail.com>
2019-12-11 17:50:50 +0100

    Add dropdown, JS controls and libraries

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@ $('.panel-collapse').on('show.bs.collapse', function () {
 $('.panel-collapse').on('hide.bs.collapse', function () {
 	$(this).siblings('.panel-heading').removeClass('active');
 });
+
+$(document).ready(function() {	
+	$('#userLastName').on('keyup', function() {
+		var $this = $(this);
+		$this.val($this.val().toUpperCase());
+	});
+
+	$('#userFirstName').on('keyup', function() {
+		var $this = $(this);
+		var str = $this.val();
+		$this.val(str[0].toUpperCase() + str.slice(1));
+	});
+
+	$('form[name=mainForm]').submit(function(event) {
+		event.preventDefault();
+
+		console.log($(this).serialize());
+
+		return false;
+	});
+
+	$('#eventParticipants').select2();
+	$('#eventType').select2();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cleaveDate = new Cleave('#lounge-date', {
+		date: true,
+		delimiter: '/',
+		datePattern: ['d', 'm', 'Y']
+	});
+
+    const cleaveTime = new Cleave('#lounge-time', {
+		time: true,
+		timePattern: ['h', 'm']
+	});
+});
